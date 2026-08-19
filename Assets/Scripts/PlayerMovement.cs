bb62301e46eaaa34e6ab804165e06287d68afe6b
ashraf <mohamedashrafece@gmail.com>
2026-08-19 04:21:37 +0300

    basic movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+    // use FixedUpdate for physics-based movement
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            // Time.deltaTime is the interval in seconds from the last frame to the current one
+            rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(0, 0, -forwardForce * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+    }
+}
